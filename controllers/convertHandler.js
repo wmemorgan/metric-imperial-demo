fe36488c94dbc226bb7fa02766c672c541908784
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-29 16:22:03 +0800

    Parse query parameters
    Identify numbers in the string
    
    Split the input by looking for the index of the first character

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,16 +7,31 @@
 */
 
 function ConvertHandler() {
-  
+  const NUMERIC_REGEXP = /[-]{0,1}[\d]*[\.]{0,1}[\d\/]+/g;
+  const ALPHA_REGEX = /[A-Za-z]+/g;
+  const round = (value, decimals) => {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+  }
+
   this.getNum = function(input) {
-    var result;
-    
+    var num = input.match(NUMERIC_REGEXP).toString()
+    console.log(`num is: `, num)
+    console.log(typeof num)
+    if ((/\//g).test(num)) {
+      var result = round(eval(num), 5)
+      console.log(`You've got a fraction the quotient is: ${result}`)
+    } else {
+      var result = input.match(NUMERIC_REGEXP)
+    }
+    console.log(`getNum result is: `, result)
     return result;
   };
   
   this.getUnit = function(input) {
-    var result;
-    
+    var firstCharIndex = input.indexOf(input.match(ALPHA_REGEX));
+    console.log(`A letter first appears in this index: `, firstCharIndex)
+    var result = input.slice(firstCharIndex)
+    console.log(`getUnit result is: `, result)
     return result;
   };
   
